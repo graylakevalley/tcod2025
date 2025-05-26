19b6f8889b580a5d0265fa502aba68b5583d920f
GrayValley <graylav12@gmail.com>
2025-05-25 21:05:46 -0400

    finished part 2 of roguelike tutorial

--- working/main.py
+++ working/main.py
@@ -3,6 +3,7 @@ import tcod
 
 from engine import Engine
 from entity import Entity
+from game_map import GameMap
 from input_handlers import EventHandler
 
 
@@ -10,6 +11,9 @@ def main() -> None:
     screen_width = 80
     screen_height = 50
     
+    map_width = 80
+    map_height = 45
+
     player_x = int(screen_width / 2)
     player_y = int(screen_width / 2)
 
@@ -22,8 +26,10 @@ def main() -> None:
     player = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255, 255, 255))
     npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255, 255, 0))
     entities = {npc, player}
+    
+    game_map = GameMap(map_width, map_height)
 
-    engine = Engine(entities=entities, event_handler=event_handler, player=player)
+    engine = Engine(entities=entities, event_handler=event_handler, game_map=game_map, player=player)
 
     with tcod.context.new_terminal(
         screen_width,
